f64ffd7a11e0613e40d2091085d892d1b0245c24
Sivan Becker <sivanbecker@gmail.com>
2020-06-14 23:57:07 +0300

    add option for larger boards

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ def cli():
     pass
 
 @cli.command()
-def play():
+@click.option('--dimension', '-d', 'd', help='choose how rows/cols will the board have assuming its a square', default=4)
+def play(d):
 
-    game = Play2048()
+    game = Play2048(d)
     game.print_board()
     game.playing()
 
--- utils.py
+++ utils.py
@@ -4,58 +4,27 @@ from collections import deque
 
 class Play2048:
 
-    def __init__(self):
-        print("Start playing 2048")
+    _WINING_NUM = 2048
+    _DIMENSION = None
+
+    def __init__(self, dimension):
+        print(f"Start playing {dimension}X{dimension} 2048")
+        self._DIMENSION = dimension
         self.valid = True # still has valid moves
-        self.ws = 16 # white spaces count
+        self.ws = self._DIMENSION**2 # white spaces count
         self.valid_keys = ['w', 'a', 's', 'd']
         self.win = False
 
-        self.board = np.full([4, 4], None)
+        self.board = np.full([self._DIMENSION, self._DIMENSION], None)
+        self.random_tile()
+        self.random_tile()
 
-        init_row_1 = random.randint(0, 3)
-        init_col_1 = random.randint(0, 3)
-        init_row_2 = random.randint(0, 3)
-        init_col_2 = random.randint(0, 3)
-
-        self.board[self.rand_cell()] = 2
-        self.board[self.rand_cell()] = 2
-        # self.board[init_row_1][init_col_1] = 2
-        # self.board[init_row_2][init_col_2] = 2
-
-        c00 = c01 = c02 = c03 = None # row0
-        c10 = c11 = c12 = c13 = None # row1
-        c20 = c21 = c22 = c23 = None # row2
-        c30 = c31 = c32 = c33 = None # row3
-
-        row0 = deque([c00, c01, c02, c03])
-        row1 = deque([c10, c11, c12, c13])
-        row2 = deque([c20, c21, c22, c23])
-        row3 = deque([c30, c31, c32, c33])
-
-        self.rows = [row0, row1, row2, row3]
-
-        col0 = [row0[0], row1[0], row2[0], row3[0]]
-        col1 = [row0[1], row1[1], row2[1], row3[1]]
-        col2 = [row0[2], row1[2], row2[2], row3[2]]
-        col3 = [row0[3], row1[3], row2[3], row3[3]]
-
-        self.cols = [col0, col1, col2, col3]
-
-
-
-        # TODO - what if initiated the same cell
-        self.rows[init_row_1][init_col_1] = 2
-        self.rows[init_row_2][init_col_2] = 2
-
-        self.ws -= 2
-
-    def print_board(self):
+    def print_board(self, msg=None):
+        if msg:
+            print(msg)
+        print('*'*10)
         print(self.board)
-        # print(self.rows[0])
-        # print(self.rows[1])
-        # print(self.rows[2])
-        # print(self.rows[3])
+        print('*'*10)
 
     def playing(self):
         while self.valid:
@@ -73,7 +42,7 @@ class Play2048:
             else:
                 self.move_right()
 
-        self.print_board()
+            self.print_board()
         if self.win:
             print("You win")
         else:
@@ -81,15 +50,20 @@ class Play2048:
 
     @staticmethod
     def rand_cell():
-        return (random.randint(0, 3), random.randint(0, 3))
+        return (random.randint(0, self._DIMENSION-1), random.randint(0, self._DIMENSION-1))
 
     def move_up(self):
         # TODO
-        print("moving up")
+        # print("moving up")
+        # self.print_board('before rotation')
         self.board = np.rot90(self.board, 1)
+        # self.print_board('after first rotation')
         self.calc_new_state()
+        # self.print_board('after calc and before second rotation')
+        self.board = np.rot90(self.board, 3)
+        # self.print_board('after second rotation')
         self.random_tile()
-        self.board = np.rot90(self.board, -1)
+
         self.is_winner()
 
     def move_down(self):
@@ -98,7 +72,7 @@ class Play2048:
         self.board = np.rot90(self.board, 3)
         self.calc_new_state()
         self.random_tile()
-        self.board = np.rot90(self.board, -3)
+        self.board = np.rot90(self.board, 1)
         self.is_winner()
 
     def move_left(self):
@@ -114,7 +88,7 @@ class Play2048:
         self.board = np.rot90(self.board, 2)
         self.calc_new_state()
         self.random_tile()
-        self.board = np.rot90(self.board, -2)
+        self.board = np.rot90(self.board, 2)
         self.is_winner()
 
 
@@ -124,7 +98,7 @@ class Play2048:
         '''
         # TODO
 
-        for i in range(4):
+        for i in range(self._DIMENSION):
             self.board[i] = self.calc_single(self.board[i])
 
 
@@ -132,39 +106,40 @@ class Play2048:
         ''' remove any None values'''
         while None in lst:
             lst.remove(None)
+
         return lst
 
     def compare(self, lst):
         ''' '''
 
         if len(lst) > 1:
-
             for i in range(len(lst)-1):
                 if lst[i] == lst[i+1]:
                     lst[i] = lst[i]*2
                     lst[i+1] = None
-
-        lst.extend([None]*(4-len(lst)))
+        lst = self.pop_n(lst)
+        lst.extend([None]*(self._DIMENSION-len(lst)))
         return lst
 
     def calc_single(self, lst):
-        ''' lst [x,y,z,w] actually a deque object
-        each of x,y,z,w can be None or int
-        '''
+
         if not any(lst): # all tiles equal None
             return lst
 
         return self.compare(self.pop_n(lst.tolist()))
 
     def is_winner(self):
-        if 2048 in self.board:
+        if self._WINING_NUM in self.board:
             self.win = True
             self.valid = False
 
+    @staticmethod
+    def random_val():
+        return 2
+
     def random_tile(self):
-        import pudb;pu.db
-        self.ws = 0
-        if self.ws == 0:
-            self.valid = False
-        else:
-            pass
+        empty_tiles = np.argwhere(self.board == None)
+        if len(empty_tiles) > 0: # no empty tiles left
+            tile = empty_tiles[random.randint(0, len(empty_tiles)-1)].tolist()
+            self.board[tile[0], tile[1]] = self.random_val()
+            self.ws -= 1
